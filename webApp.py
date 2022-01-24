f5fb5d3a5875426f50efec4ea27c3cb9cfcb78b9
Ashwith Poojary <pashwith25@gmail.com>
2022-01-24 14:28:56 +0530

    - added speech to sign translation - modified requirements file

--- webApp.py
+++ webApp.py
@@ -1,3 +1,4 @@
+from turtle import right
 import keras
 from matplotlib.style import use
 from HandTrackingModule import HandDetector
@@ -8,6 +9,8 @@ import cv2
 
 import numpy as np
 import streamlit as st
+
+import speech_recognition as sr
 
 from streamlit_webrtc import (
     AudioProcessorBase,
@@ -23,6 +26,27 @@ import copy
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=2)
+        with st.spinner('Listening...'):
+            print("Listening...")
+            r.pause_threshold = 1
+            audio = r.listen(source)
+    try:
+        with st.spinner('Recognising...'):
+            print("Recognising...")
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Say that again please...")  
+        return None
+
+    return query
 
 def main():
     st.header("Indian Sign Language Translation using Human-Computer Interaction")
@@ -42,43 +66,62 @@ def main():
     if app_mode == option1:
         sign_language_detector()
     elif app_mode == option2:
-        text_to_sign()
+        text_speech_to_sign()
 
-def text_to_sign():
+def signGenerator(statement):
+    words = statement.split()
+    words = [word.upper() for word in words if word not in '.,!?:;']
+    for word in words:
+        if word.lower() in ['little', 'hi']:
+            st.write(word.upper())
+            st.image('./signs/{}.jpg'.format(word.capitalize()), width=75)
+        else:
+            imageList = []
+            for letter in word:
+                if letter not in ',.!?;:()':
+                    imageList.append(cv2.resize(cv2.imread('./signs/{}.jpg'.format(letter)), (75, 75)))
 
-    st.markdown(f'<h1 style="color:#33ff33; font-size:24px;">{"Text to Sign Language"}</h1>', unsafe_allow_html=True)
+            st.write(word)
+            st.image(cvzone.stackImages(imageList, 8 if len(imageList) >= 8 else len(imageList), 1), width=75*(8 if len(imageList) >=8 else len(imageList)))
 
-    text = st.text_area("Enter the text to be translated", "")
-    text = text.lower()
-    if st.button("Translate"):
+def text_speech_to_sign():
 
-        # if 'i love you' in text:
-        #     text = text.replace('i love you', '')
-        #     st.write('I LOVE YOU')
-        #     st.image('./signs/I Love You.jpg', width=75)
+    st.markdown(f'<h1 style="color:#33ff33; font-size:24px;">{"Text/Speech to Sign Language"}</h1>', unsafe_allow_html=True)
 
-        words = text.split()
-        words = [word.upper() for word in words if word not in '.,!?:;']
-        for word in words:
-            if word.lower() in ['little', 'hi']:
-                st.write(word.upper())
-                st.image('./signs/{}.jpg'.format(word.capitalize()), width=75)
+    option = st.selectbox(
+     'How would you like to convey the message?',
+     ('Text to Sign Language', 'Speech to Sign Language'))
+
+    st.write('You selected:', option)
+
+    if option == 'Text to Sign Language':
+        text = st.text_area("Enter the text to be translated", "")
+        text = text.lower()
+
+        if st.button("Translate", key='translate'):
+            # if 'i love you' in text:
+            #     text = text.replace('i love you', '')
+            #     st.write('I LOVE YOU')
+            #     st.image('./signs/I Love You.jpg', width=75)
+
+            signGenerator(text)
+
+            # try:
+            #     image = Image.open('./signs/{}.jpg'.format(text))
+            #     st.success(f"Translated text is: ")
+            #     st.image(image, caption='Predicted sign')
+            # except:
+            #     st.error("No sign recognised")
+    else:
+        if st.button('Record', key='record'):
+            query = takeCommand()
+
+            if query is None:
+                st.error("Speech recognition failed, Please try again")
             else:
-                imageList = []
-                for letter in word:
-                    if letter not in ',.!?;:()':
-                        imageList.append(cv2.resize(cv2.imread('./signs/{}.jpg'.format(letter)), (75, 75)))
-
-                st.write(word)
-                st.image(cvzone.stackImages(imageList, 8 if len(imageList) >= 8 else len(imageList), 1), width=75*(8 if len(imageList) >=8 else len(imageList)))
-
-        # try:
-        #     image = Image.open('./signs/{}.jpg'.format(text))
-        #     st.success(f"Translated text is: ")
-        #     st.image(image, caption='Predicted sign')
-        # except:
-        #     st.error("No sign recognised")
-        
+                st.write("You said: ")
+                st.success(query)
+                signGenerator(query)
 
 def sign_language_detector():
     st.markdown(f'<h1 style="color:#33ff33; font-size:24px;">{"Sign Language to Text/Speech"}</h1>', unsafe_allow_html=True)
